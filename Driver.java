f0c71b5664e4f239caf082814789eead27daf377
RakinAB <142949864+RakinAB@users.noreply.github.com>
2024-01-16 00:39:12 -0600

    commit username check

--- Driver.java
+++ Driver.java
@@ -9,38 +9,67 @@ import java.io.PrintWriter;
 public class Driver {
 
     public static void main(String[] args) {
-        boolean done = false;   //keeps the loop going
-        User currentUser;       //whoever is logged in
+        boolean done = false;           //keeps the loop going
+        User currentUser = null;       //whoever is logged in
         int userCount = 1;
 
         while(!done){
             Scanner scan = new Scanner(System.in);
 
             System.out.println("Welcome to the Mock Social Media Platform");
-            System.out.println("================================================================================================\n");
+            System.out.println("========================================================================\n");
             System.out.println("Please select an option:");
             System.out.println("(A) Log in as Existing User");
             System.out.println("(B) Create an Account");
             System.out.println("(C) Exit");
-            String option = scan.nextLine();
 
-            boolean validChoice = false;
-            while(!validChoice){
+            boolean doneMenu = false;
+            while(!doneMenu){
+                String option = scan.nextLine();
+                //Logging in
                 if(option.equalsIgnoreCase("A")){
+                    System.out.println("========================================================================\n");
                     System.out.println("Log In");
-                    validChoice = true;
+                    doneMenu = true;
                 }
+                //Creating account
                 else if(option.equalsIgnoreCase("B")){
-                    System.out.println("Creating Account");
-                    validChoice = true;
+                    String username = "";
+                    String password = "";
+                    String email = "";
+
+                    System.out.println("========================================================================\n");
+
+                    //Username
+                    boolean validName = false;
+                    while(!validName){
+                        System.out.println("Enter your Username (Must be more than 3 characters): ");
+                        String input = scan.next();
+                        if((input.length() <= 3)){
+                            System.out.println("Invalid Try again");
+                        } else{
+                            validName = true;
+                        }
+                    }
+                    System.out.println();
+
+                    //Password
+                    boolean validPassword = false;
+                    while(!validPassword){
+                        
+                    }
+                    doneMenu = true;
+
                 }
+                //Leaving
                 else if(option.equalsIgnoreCase("C")){
+                    System.out.println("========================================================================\n");
                     System.out.println("Leaving");
-                    validChoice = true;
+                    doneMenu = true;
                     done=true;
                 }
                 else{
-                    System.out.println("invalid");
+                    System.out.println("Invalid Try Again....");
                 }
             }
 
